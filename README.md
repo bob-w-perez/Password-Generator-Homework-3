3569716bf4036bd6b1d4e98352db3c10c9146601
bob.w.perez <bob.w.perez@gmail.com>
2021-06-18 17:42:16 -0400

    descriptive comments added to JS file

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Password-Generator-Homework-3
+
+- This project was to make a a random password generator where the user can choose the criteria (length and which types of characters to use) and then have the program display the password. The design and layout was provided as base code; the project consisted of programming the functionality in JavaScript.
+## Installation
+
+No installation required.
+Go to [https://bob-w-perez.github.io/Portfolio-Homework-2/](https://bob-w-perez.github.io/Portfolio-Homework-2/) to use the application.
+
+The code and all relevant files can be found at [https://github.com/bob-w-perez/Portfolio-Homework-2](https://github.com/bob-w-perez/Portfolio-Homework-2). 
+
+## Usage
+
+
+![screenshot](https://github.com/bob-w-perez/Portfolio-Homework-2/blob/main/assets/images/screenshot.gif "Screenshot")
+
+
+
+
+
+## Contributing
+Solo project for GATech Coding Bootcamp
+
+## License
+MIT License
+
+Copyright (c) 2021 Robert Perez
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,121 +1,144 @@
 // Assignment Code
+// set a variable for the generate button
 var generateBtn = document.querySelector("#generate");
+
+// set arrays for each class of characters for potential password criteria
 var lowerLetters = [ 'a','b','c','d','e','f','g','h','i','j',
                     'k','l','m','n','o','p','q','r',
                   's','t','u','v','w','x','y','z' ];
-
 var upperLetters = [];
-
 for (i=0; i < lowerLetters.length; i++) {
     upperLetters.push(lowerLetters[i].toUpperCase())
 }
 
-
 var numList = ['1','2','3','4','5','6','7','8','9','0'];
-
 var specChars = [' ', '!','"','#','$','%','&',"'",'(',')','*','+',',','-',
                   '.','/',':',';','<','=','>','?','@','[', '\\',  
                   ']', '^','_','`','{','|','}','~'];
 
+// sets empty array which will contain one or more of the above
+// arrays based on selected criteria. These will be concatenated
+// into a single array
 var passOptions = [];
+
+// sets empty array which will contain one or more of the above
+// arrays based on selected criteria. These will NOT be concatenated
+// and will be an array with other arrays as elements.
+// This will function to check if the created password has at least
+// one character from each class
 var passCheck = [];
+
+// sets empty string which the makePassword() function will add
+// random characters onto
 var madePassword = "";
 
 
-
-
+// this function takes care of getting user input to set up the correct
+// set of characters to make the password
 function generatePassword () {
 
+  //resets these to empty arrays if the user wants to run it multiple times
   passOptions = [];
   passCheck = [];
 
+  // prompts user for length of password
   var passLen = prompt("Enter password length (must be between 8 and 128):");
   
-
+// if the user presses cancel for the prompt above the function ends
   if (!passLen) {
     return;
   }
+
+  // if the user presses ok but does not input anything the function ends
   if (passLen == undefined ) {
     return;
   }
 
+  // converts string from prompt input to a number
   passLen = Number(passLen);
+  
+  // if the user inputs something that is not a number they will be prompted to try again
   if (isNaN(passLen)) {
     alert("Your input was not a number. Please try again.");
-    generatePassword();
     return;
   }
 
+  // checks to make sure the length is between 8 and 128
   if (passLen < 8 || passLen > 128) {
     alert("Password length must be between 8 and 128. Please try again.");
-    generatePassword();
     return;
   }
 
+  // prompts user if they want lowercase letters
   if (confirm("Include lower case letters?")) {
+    // if true concatenates lowercase array to passOptions
     passOptions = passOptions.concat(lowerLetters);
+    // adds the whole array of lowercase as an element to passCheck
     passCheck.push(lowerLetters);
   }
 
+  // functionally the same as above except for uppercase
   if (confirm("Include upper case letters?")) {
     passOptions = passOptions.concat(upperLetters);
     passCheck.push(upperLetters);
-
   }
 
+  // functionally the same as above except for numeric
   if (confirm("Include numbers?")) {
     passOptions = passOptions.concat(numList);
     passCheck.push(numList);
-
   }
 
+  // functionally the same as above except for special characters
   if (confirm("Include special characters? ")) {
     passOptions = passOptions.concat(specChars);
     passCheck.push(specChars);
-
   }
 
+  // if the user does not select any of the criteria options they are prompted to 
+  // try again
   if (passOptions.length == 0) {
     alert("You must select at least one character type. Please try again.");
-    generatePassword();
     return;
   }
 
+  // the function ends by calling the makePassword() function and passing the length to it
+  // as a parameter. the other relevant variables are globally scoped and do not need to be passed
   makePassword(passLen);
 }
 
-function findCommonElements(arr1, arr2) {
-  return arr1.some(item => arr2.includes(item));
+// this function takes 2 arrays as inputs and returns TRUE if at least one element of array1 
+// exists in array2 
+function findCommon(array1, array2) {
+  return array1.some(i => array2.includes(i));
 }
 
+// this function chooses random elements from the passOptions array to make a random password
 function makePassword(passLen) {
-    var newLen = passLen;
+    // resets madePassword to empty string
     madePassword = "";
+
+    // loops for the number of times equal to passLen and each time adds a random character to madePassword
     for (i = 0; i < passLen; i++) {
       madePassword = madePassword.concat(passOptions[Math.floor(Math.random()*passOptions.length)]);
     }
     
-    console.log(madePassword)
 
+    // loops for the number of character classes chosen and runs findCommon for each class
+    // to ensure at least one character from each chosen class is in the password
     for (i = 0; i < passCheck.length; i++){
-      if(!findCommonElements(madePassword.split(''), passCheck[i])){
-          makePassword(newLen);
+      if(!findCommon(madePassword.split(''), passCheck[i])){
+        // if madePassword does not contain a character from each class, the makePassword() function
+        // is run recursively until it satisfies this criteria
+          makePassword(passLen);
           return;
       }
     }
     return madePassword;
   }
    
-function resetVal() {
-  console.log("TESTING");
-  document.querySelector("#password").value = '';
-  document.querySelector("#password").blur();
-}
-
 // Write password to the #password input
 function writePassword() {
-  resetVal();
-  
   generatePassword();
   var passwordText = document.querySelector("#password");
   var password = madePassword;
@@ -124,8 +147,7 @@ function writePassword() {
   }
   
 }
-var testClick = document.querySelector(".card-header");
-testClick.addEventListener("click", resetVal);
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
